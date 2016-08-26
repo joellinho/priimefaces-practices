cfef6f0d7dd5b3bb66921b220cef95a87f3b9e05
joel <joelg_es@hotmail.com>
2016-08-26 16:40:08 -0500

    dd

---
+++ onliness/Onlinestore2/src/java/com/pe/online/entity/Usuario.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pe.online.entity;
+
+
+public class Usuario {
+   private int codigo;
+   private String usuario;
+   private String password;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+}
